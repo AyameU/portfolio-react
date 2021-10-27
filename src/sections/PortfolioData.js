a9fa02314cc5dea9d4fe9dfad90f287324eee88d
AyameU <54376512+AyameU@users.noreply.github.com>
2021-10-27 01:51:52 -0500

    Add new 'Hello, Trivia' portfolio section

--- src/sections/PortfolioData.js
+++ src/sections/PortfolioData.js
@@ -1,6 +1,14 @@
 const Projects = [
     {
         "id": "project1",
+        "name": "Hello, Trivia",
+        "category": "Website + Trivia Game // React, Bulma",
+        "text": "Do you want to play a game? A React-based trivia game based on questions from the Open Trivia DB API. Styled with Bulma.",
+        "link": "https://ayameu.github.io/HelloTrivia/",
+        "image": "",
+    },
+    {
+        "id": "project2",
         "name": "Historical Resources",
         "category": "Website // Open Data, JS, HTML, Bulma",
         "text": "Uses the City of Winnipeg's Historical Resources Open Data API to retrieve records based on a street address.",
@@ -8,7 +16,7 @@ const Projects = [
         "image": "",
     },
     {
-        "id": "project2",
+        "id": "project3",
         "name": "RTX Unicorn",
         "category": "Website // HTML, CSS, JS",
         "text": "Parody website for an ultra rare GPU line-up. A tongue-in-cheek take on the state of the GPU market.",
@@ -16,7 +24,7 @@ const Projects = [
         "image": "",
     },
     {
-        "id": "project3",
+        "id": "project4",
         "name": "Personal Portfolio v1",
         "category": "Website",
         "text": "This is the first version of my personal portfolio. Built with HTML, CSS and a wee bit of JavaScript.",
